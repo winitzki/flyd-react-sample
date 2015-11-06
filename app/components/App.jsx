0ae8e1d3529d9f29fe3b59a4f1bae3fbc40a9d96
Sergei Winitzki <sergei@versal.com>
2015-11-06 12:44:32 -0800

    minor changes before demo

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -1,11 +1,7 @@
 import React from 'react'
 import F from 'flyd'
 
-import {model_stream, input_stream, UserClickT} from '../model'
-
-const coffee = () => input_stream(UserClickT.MakeCoffee())
-const commit = () => input_stream(UserClickT.CommitCode())
-const restart = () => input_stream(UserClickT.Restart())
+import {model_stream, input_stream, coffee, commit, restart} from '../model'
 
 export default class App extends React.Component {
 
@@ -17,11 +13,11 @@ export default class App extends React.Component {
 
     return <div className="container">
       <p className="title">A coder's working day</p>
+      <button onClick={restart}>Restart</button>
       <button onClick={coffee}>Make coffee</button>
-      {this.state.can_commit ? <button onClick={commit}>Commit code</button> : undefined}
       <span className="lines_of_code">Committed: {this.state.committed}</span>
       <span className="lines_of_code">Uncommitted: {this.state.uncommitted}</span>
-      <button onClick={restart}>Restart</button>
+      {this.state.can_commit ? <button onClick={commit}>Push a commit</button> : undefined}
     </div>
   }
 
--- app/model.js
+++ app/model.js
@@ -1,19 +1,23 @@
 import F from 'flyd'
-import T from './union-type'
+import T from 'union-type'
 import R from 'ramda'
 
 // input stream
 
-const UserClickT = T({ MakeCoffee: [], CommitCode: [], Restart: [] })
+const UserClickT = T({ MakeCoffee: [], PushCommit: [], Restart: [] })
 
 const input_stream = F.stream(UserClickT.Restart()) // values of type UserClickT
+
+const coffee = () => input_stream(UserClickT.MakeCoffee())
+const commit = () => input_stream(UserClickT.PushCommit())
+const restart = () => input_stream(UserClickT.Restart())
 
 // model
 
 const init_model = { can_commit: false, uncommitted: 0, committed: 0 }
 
 const make_new_model = (old_model, click) => UserClickT.case({
-  CommitCode: () => ({
+  PushCommit: () => ({
     can_commit: false,
     committed: old_model.committed + old_model.uncommitted,
     uncommitted: 0
@@ -34,5 +38,7 @@ const model_stream = F.scan(make_new_model, init_model, input_stream)
 module.exports = {
   model_stream,
   input_stream,
-  UserClickT
+  coffee,
+  commit,
+  restart
 }
